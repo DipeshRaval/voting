f6abe959ed258f6c372d4283c7615c19435f5962
DipeshRaval <dipeshraval50@gmail.com>
2023-01-03 23:32:21 +0530

    change port to env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("LIstening at 3000");
+app.listen(port, () => {
+  console.log(`server listening at port - ${port}`);
 });
