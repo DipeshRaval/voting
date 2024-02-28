422ff8c3756607c9be254f72b9aa28be517d398e
DipeshRaval <dipeshraval50@gmail.com>
2024-02-28 23:55:29 +0530

    chnages in index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Dipesh server listening at port - ${port}`);
+  console.log(`server listening at port - ${port}`);
 });
